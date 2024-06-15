92076806a71bc3d398c89655d2e3f11efeac2916
TahaDahech <56277969+TahaDahech@users.noreply.github.com>
2024-06-16 01:06:05 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,6 @@ import 'package:flutter/material.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: const FirebaseOptions(
-          apiKey: "AIzaSyDWKZd29xncdLubNCQHeqPtTzvlzSG3LWU",
-          authDomain: "butterflies-7fea8.firebaseapp.com",
-          projectId: "butterflies-7fea8",
-          storageBucket: "butterflies-7fea8.appspot.com",
-          messagingSenderId: "537373805092",
-          appId: "1:537373805092:web:e84b80d3ed8f7f2f8266b1"));
+      options: const FirebaseOptions());
   runApp(const MyApp());
 }
